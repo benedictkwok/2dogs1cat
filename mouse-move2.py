9bd7bd4109494b8d38cb0f02ae06e029836e9916
benkwok <benedictkwok@yahoo.com>
2018-12-11 15:02:23 -0800

    Add files via upload

---
+++ mouse-move2.py
@@ -0,0 +1,21 @@
+import pyautogui
+import time
+print('Press Contrl-C to quit')
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionStr='X: ' +str(x).rjust(4) + ' Y: '+str(y).rjust(4)
+        print(positionStr)
+        time.sleep(120)
+        new_x, new_y = pyautogui.position()
+
+        if x==new_x:
+            pyautogui.click(500,100)
+            pyautogui.typewrite('1')
+            time.sleep(1)
+            #pyautogui.moveRel(10, 0, duration=0.25)
+            #pyautogui.moveRel(-10, 0, duration=0.25)
+            #time.sleep(1)
+
+except KeyboardInterrupt:
+    print('\nBye!')
